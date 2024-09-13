12dcd89ad27304adb890ebc39922515ffc496d77
pauuulaaa <heleinapaula@gmail.com>
2024-09-13 16:43:52 +0800

    Create paulamolina variables.js

---
+++ paulamolina variables.js
@@ -0,0 +1,9 @@
+let name = "Heleina Paula G. Molina";
+let whatDoYouWannaBecomeinYourLife = "programmer";
+let gender = "male";
+let twitterHandle = "@heleinapaula";
+
+console.log(' My name is ', name);
+console.log('I want to become a ', whatDoYouWannaBcomeinYourLife);
+console.log('My gender is ;, gender);
+console.log('My twitter handle is ', twitterhandle);
